93f6674d471fd6e0715a43dea6995321ed3a6274
Chai <chai1998@live.com>
2019-12-05 18:23:35 +0800

    Display multiple GPU and RAM size
    formatted with Pretty and using jQuery

--- firebase/public/app.js
+++ firebase/public/app.js
@@ -1,31 +1,36 @@
-const pclist = document.querySelector('#pc-list');
+const pclist = $('#pc-list');
 
-//Render the PC list 
-function renderPC(doc) {
-    let li = document.createElement('tr');
-    let pc_name = document.createElement('td');
-    let cpu = document.createElement('td');
-    let model = document.createElement('td');
+function concatObj(obj, arrayKey) {
+    var temp = [];
 
-    //Set the data-id tag fot the li
-    li.setAttribute('data-id', doc.id);
-    //Put the value into the variable 
-    pc_name.textContent = doc.id;
-    cpu.textContent = doc.data().CPU;
-    model.textContent = doc.data().Model;
+    Object.entries(obj).forEach(([key, val]) => temp.push(val[arrayKey]));
 
-
-    //Append all of the span tag into li tag
-    li.appendChild(pc_name);
-    li.appendChild(cpu);
-    li.appendChild(model);
-    //Append all of the li tag to the ul tag
-    pclist.appendChild(li);
+    return temp.join(', ');
 }
 
+function renderPC(doc) {
+    const data = doc.data();
+    console.log(data);
 
-db.collection('labs').doc('TL04-01').collection('computers').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderPC(doc);
-    })
-})
+    var dom = `
+        <tr data=id=${doc.id}>
+            <td>${doc.id}</td>
+            <td>${data.CPU}</td>
+            <td>${data.Model}</td>
+            <td>${concatObj(data.GPUs, 'Description')}</td>
+            <td>${concatObj(data.RAMs, 'Capacity')}</td>
+        </tr>
+    `;
+
+    pclist.append(dom);
+}
+
+db.collection('labs')
+    .doc('TL04-01')
+    .collection('computers')
+    .get()
+    .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            renderPC(doc);
+        });
+    });
