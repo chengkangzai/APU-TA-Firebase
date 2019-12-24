c0ed054385b18832975b4acfdd979be36b1064cd
chengkangzai <pycck@hotmail.com>
2019-12-25 00:56:02 +0800

    Code efficiency

--- firebase/public/Lab/app.js
+++ firebase/public/Lab/app.js
@@ -5,19 +5,15 @@ var url_string = window.location.href; // window.location.href
 var url = new URL(url_string);
 var lab = url.searchParams.get("lab");
 
-
 function concatObj(obj, arrayKey) {
     var temp = [];
-
     Object.entries(obj).forEach(([key, val]) => temp.push(val[arrayKey]));
-
     return temp.join(', ');
 }
 
 function renderPC(doc) {
     const data = doc.data();
     console.log(data);
-
     var dom = `
         <tr data-id=${doc.id }>
             <td>${doc.id}</td>
@@ -26,14 +22,10 @@ function renderPC(doc) {
             <td>${concatObj(data.GPUs, 'Name')}</td>
             <td>${concatObj(data.RAMs, 'Capacity')}</td>
             <td>${concatObj(data.Storage, 'Size')}</td>
-
         </tr>
     `;
-
     pclist.append(dom);
 }
-
-
 
 db.collection('labs').doc(lab).collection('computers').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
