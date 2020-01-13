6d34e960ce0483bccf0fe6e1692f950cdd2505af
chengkangzai <pycck@hotmail.com>
2020-01-13 23:48:10 +0800

    improve Feedback

--- firebase/public/Feedback/read.js
+++ firebase/public/Feedback/read.js
@@ -11,16 +11,34 @@ function renderFeedback(doc) {
     const data = doc.data();
     console.log(data);
 
+    var timestamp="";
+    if (!data.timestamp) {
+        timestamp="n/a";
+    }else{
+        var time=data.timestamp.toDate();
+        timestamp= new Date(time);
+        timestamp=timestamp.toLocaleDateString('en-GB');
+    }
+
     var dom = `
-        <span data-id=${doc.id } class="list-group-item">
-            <li>${data.feedback}</li>
-        </span>
+    <tr id="${doc.id}">
+        <td>${data.feedback}</td>
+        <td>${timestamp}</td>
+        <td><a class="btn btn-danger text-white" onclick="deleteEntry('${doc.id}');">delete </a></td>
+    </tr>
     `;
 
     feedbacklist.append(dom);
 }
 
-
+function deleteEntry(docId){
+    db.collection("feedback").doc(docId).delete().then(function() {
+        console.log("Document successfully deleted!");
+        $("#"+docId).hide();
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
 
 
 
@@ -28,4 +46,12 @@ db.collection('feedback').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderFeedback(doc);
     });
+}).catch(function(error) {
+    console.error("Error writing document: ", error);
+    console.log (error.code);
+    if (error.code == "permission-denied") {
+        alert("You have no permission to view the page");
+        window.history.go(-1);
+        
+    }
 });
