fd34434953881dab97023284eaa1bb170d23458a
chengkangzai <pycck@hotmail.com>
2020-04-28 01:03:28 +0800

    Firebase UI for credential management :art:

--- firebase/public/app.js
+++ firebase/public/app.js
@@ -1,20 +1,34 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) window.location.href = "Lab/index.html";
-});
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            //document.getElementById('loader').style.display = 'none';
+            $("#loader").hide();
+        }
+    },
+    'credentialHelper': firebaseui.auth.CredentialHelper.NONE,
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '/firebase/public/Lab/index.html',
+    signInOptions: [{
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    }],
 
-//Incase ppl want to login by enter
+};
+ui.start('#authContainer', uiConfig);
 
-function checkIfIsEnter(event) {
-    if (event.keyCode == 13) login();
-}
+//TODO
+// Email link login
 
-function login() {
-    var email = $("#email").val();
-    var password = $("#password").val();
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function(response) {})
-        .catch(function(error) {
-            alert(error);
-        });
-}
+particleJS();
+makeFloatOnParticle("authContainer");
