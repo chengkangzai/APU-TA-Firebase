fe460b8e1231ba3cedf4fbffe3386dd0faf7861b
chengkangzai <pycck@hotmail.com>
2020-04-28 14:04:19 +0800

    code efficiency  :zap: :zap: :zap:

--- firebase/public/Lab/collapse.js
+++ firebase/public/Lab/collapse.js
@@ -1,72 +1,75 @@
 //Button
-var y   = $('#openLevel3');
-var y1  = $('#openLevel4');
-var y2  = $('#openLevel5');
-var y3  = $('#openLevel6');
-var y4  = $('#open_Apiit');
+var y = $('#openLevel3');
+var y1 = $('#openLevel4');
+var y2 = $('#openLevel5');
+var y3 = $('#openLevel6');
+var y4 = $('#open_Apiit');
 
 
 //Hiiden 
-var Z   = $('#level-3');
-var Z1  = $('#level-4');
-var Z2  = $('#level-5');
-var Z3  = $('#level-6');
-var Z4  = $('#APIIT');
+var Z = $('#level-3');
+var Z1 = $('#level-4');
+var Z2 = $('#level-5');
+var Z3 = $('#level-6');
+var Z4 = $('#APIIT');
 
-Z.hide();
-Z1.hide();
-Z2.hide();
-Z3.hide();
-Z4.hide();
+var Zs = [Z, Z1, Z2, Z3, Z4];
 
+hideAllSection();
 
 //Welcome Page
 var a = $('#welcome');
 
+function hideAllSection() {
+    Zs.forEach(element => {
+        element.hide();
+    });
+}
+
 function openlevel3() {
-    Z.show();
-    Z1.hide();
-    Z2.hide();
-    Z3.hide();
-    Z4.hide();
-    a.hide();
+    if (Z.css('display') === "none") {
+        hideAllSection();
+        Z.show();
+        a.hide();
+    }
 }
 
 function openlevel4() {
-    Z1.show();
-    Z.hide();
-    Z2.hide();
-    Z3.hide();
-    Z4.hide();
-    a.hide();
+    if (Z1.css('display') === "none") {
+        hideAllSection();
+        Z1.show();
+        a.hide();
+    } else {
+        hideAllSection();
+    }
 }
 
 function openlevel5() {
-    Z2.show();
-
-    Z.hide();
-    Z3.hide();
-    Z1.hide();
-    Z4.hide();
-    a.hide();
+    if (Z2.css('display') === "none") {
+        hideAllSection();
+        Z2.show();
+        a.hide();
+    } else {
+        hideAllSection();
+    }
 }
 
 function openlevel6() {
-    Z3.show();
-
-    Z.hide();
-    Z1.hide();
-    Z2.hide();
-    Z4.hide();
-    a.hide();
+    if (Z3.css('display') === "none") {
+        hideAllSection();
+        Z3.show();
+        a.hide();
+    } else {
+        hideAllSection();
+    }
 }
 
 function open_apiit() {
-    Z4.show();
-
-    Z.hide();
-    Z1.hide();
-    Z2.hide();
-    Z3.hide();
-    a.hide();
+    if (Z4.css('display') === "none") {
+        hideAllSection();
+        Z4.show();
+        a.hide();
+    } else {
+        hideAllSection();
+    }
 }
